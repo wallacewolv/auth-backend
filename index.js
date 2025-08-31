b6c126029c4b694a041df1cec6c387159bfd66ee
Wallace W <wallace_wolv@hotmail.com>
2025-08-30 21:04:55 -0300

    feat: Criando rotas e controller para  auth

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-import express from 'express';
 import dotenv from 'dotenv';
+import express from 'express';
+
 import { connectDB } from './db/connect-db.js';
+import { authRoutes } from './routes/auth.routes.js';
 
 dotenv.config();
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send({ status: 'OK'});
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   connectDB();
---
+++ routes/auth.routes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+import { login, logout, signup } from '../controllers/auth.controller.js';
+
+export const authRoutes = express.Router();
+
+authRoutes.get("/signup", signup);
+authRoutes.get("/login", login);
+authRoutes.get("/logout", logout);
