d2a90ad1fd101c59659e9ca6b1206cbe28f2e071
Wallace W <wallace_wolv@hotmail.com>
2025-09-03 00:14:20 -0300

    feat: Implementando funcionalidade para rota de logout

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -104,5 +104,9 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-  res.send("Logout route");
+  res.clearCookie("token");
+  res.status(200).json({
+    success: true,
+    message: "Logged out successfully",
+  });
 };
