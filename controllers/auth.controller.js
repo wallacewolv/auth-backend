975df5c5c67e3fe1240e28e96e72310b05bcc1cb
Wallace W <wallace_wolv@hotmail.com>
2025-09-03 00:14:34 -0300

    feat: Implementando funcionalidade para rota de login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -100,7 +100,47 @@ export const verifyEmail = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("Login route");
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    generateTokenAndSetCookie(res, user._id);
+
+    user.lastLogin = Date.now();
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log(`Error in login: ${error}`);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
 };
 
 export const logout = async (req, res) => {
