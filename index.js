e5cb035a14740a7baf3d1327583b73185f1841b4
Wallace W <wallace_wolv@hotmail.com>
2025-09-06 21:12:19 -0300

    feat: Configurando cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 
 import { connectDB } from "./db/connect-db.js";
 import { authRoutes } from "./routes/auth.routes.js";
@@ -9,6 +10,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 app.use(express.json()); // allows us to parse incoming requests: req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
